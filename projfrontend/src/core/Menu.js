d1f3c82601fad0fb406a1cdefb6d64fda28f523c
Kartik Gandhi <kartikgandhi76@gmail.com>
2023-02-06 02:28:55 +0530

    commit

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 // import { withRouter } from "react-router-dom";
+
 import {
   useLocation,
   useNavigate,
@@ -24,13 +25,25 @@ function withRouter(Component) {
   return ComponentWithRouterProp;
 }
 
-const Menu = ()=> {
+const currentTab = (history,path)=>{
+  if(window.location.pathname === path){
+    return {color:"#2ecc72"}
+  }else{
+    return {color: "#FFFFFF"}
+  }
+}
+
+const Menu = ({history})=> {
   return (
     <div>
         <ul className="nav nav-tabs bg-dark">
-            <li className="nav-item">
-                <Link className='nav-link' to="">Home</Link>
-            </li>
+            <li className="nav-item"><Link style={currentTab(history,"/")} className='nav-link' to="/">Home</Link></li>
+            <li className="nav-item"><Link style={currentTab(history,"/cart")} className='nav-link' to="/cart">Cart</Link></li>
+            <li className="nav-item"><Link style={currentTab(history,"/user/dashboard")} className='nav-link' to="/user/dashboard">Dashboard</Link></li>
+            <li className="nav-item"><Link style={currentTab(history,"/admin/dashboard")} className='nav-link' to="/admin/dashboard">A. Dashboard</Link></li>
+            <li className="nav-item"><Link style={currentTab(history,"/signup")} className='nav-link' to="/signup">Signup</Link></li>
+            <li className="nav-item"><Link style={currentTab(history,"/signin")} className='nav-link' to="/signin">Signin</Link></li>
+            <li className="nav-item"><Link style={currentTab(history,"/signout")} className='nav-link' to="/signout">Signout</Link></li>
         </ul>
     </div>
   )
