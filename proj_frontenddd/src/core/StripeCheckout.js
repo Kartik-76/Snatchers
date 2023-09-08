ef0313b7f1db3fb0aae6d01fae9608c80fc71357
Kartik Gandhi <Kartikgandhi76@gmail.com>
2023-09-09 00:11:30 +0530

    Stripe Integrated

--- proj_frontenddd/src/core/StripeCheckout.js
+++ proj_frontenddd/src/core/StripeCheckout.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import { isAuthenticated } from "../auth/helper";
 import { cartEmpty, loadCart } from "./helper/cartHelper";
 import { Link } from "react-router-dom";
+import StripeCheckoutButton from "react-stripe-checkout";
+import { API } from "../backend";
+import { createOrder } from "./helper/orderHelper";
+
 
 const StripeCheckout = ({
   products,
@@ -26,12 +30,44 @@ const StripeCheckout = ({
     return amount;
   };
 
+  const makePayment = token => {
+    const body = {
+      token,
+      products
+    };
+    const headers = {
+      "Content-Type": "application/json"
+    };
+    return fetch(`${API}/stripepayment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        console.log(response);
+        //call further methods
+        const {status} = response;
+        console.log("STATUS ", status);
+        cartEmpty()
+      })
+      .catch(error => console.log(error));
+  };
+
   const showStripeButton = () => {
     return isAuthenticated() ? (
-      <button className="btn btn-success">Pay with stripe</button>
+      <StripeCheckoutButton
+        stripeKey="pk_test_51NnQNfSBOx3GOH3J8H4CZ0Xn79yH4oOCkvx3Mw5IeqSChVPadWfcqou9xd70wNn7gSYGFspxFToKBmiNAMl90Rvi009O4mOARa"
+        token={makePayment}
+        amount={getFinalAmount() * 100}
+        name="Buy Tshirts"
+        shippingAddress
+        billingAddress
+      >
+        <button className="btn btn-success">Pay with stripe</button>
+      </StripeCheckoutButton>
     ) : (
       <Link to="/signin">
-        <button className="btn btn-warning">Signin</button>
+        <button className="btn btn-warning">SignIn</button>
       </Link>
     );
   };
--- projbackend/app.js
+++ projbackend/app.js
@@ -7,12 +7,14 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+
 //myroutes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripepayment")
 
 
 //DB Connection
@@ -37,6 +39,7 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api",orderRoutes);
+app.use("/api",stripeRoutes);
 
 
 //Port
